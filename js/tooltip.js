20607e5aea3e848d4b387c61059fd99d00eebf74
skrudra <skrudra12@gmail.com>
2019-12-09 21:45:36 +0530

    fade-in fade-out effect

--- js/tooltip.js
+++ js/tooltip.js
@@ -3,21 +3,51 @@
  */
 
 let setUpToolTip = function() {
-	let tooltip = "", toolTipDiv = document.querySelector(".div-tool-tip"),
-	toolTipElements = Array.from(document.querySelectorAll(".tool-tip"));
+	let tooltip = "", toolTipDiv = document.querySelector(".div-tool-tip"), toolTipElements = Array
+			.from(document.querySelectorAll(".tool-tip"));
 
 	let displayTooltip = function(e, obj) {
 		tooltip = obj.dataset.tooltip;
 		toolTipDiv.innerHTML = tooltip;
-		toolTipDiv.style.opacity = 1;
 		toolTipDiv.style.top = e.pageY + "px";
 		toolTipDiv.style.left = e.pageX + "px";
+		// toolTipDiv.style.opacity = 1;
+		fadeIn(toolTipDiv);
 
+	};
+
+	let fadeOut = function(element) {
+		let op = 1;
+		let timer = setInterval(function() {
+			if (op <= 0.1) {
+				clearInterval(timer);
+				element.style.opacity = 0;
+				element.style.display = "none";
+			}
+			element.style.opacity = op;
+			op -= op * 0.1;
+		}, 10);
+	};
+
+	let fadeIn = function(element) {
+		let op = 0.1;
+		element.style.display = "block";
+		let timer = setInterval(function() {
+			if (op >= 1) {
+				clearInterval(timer);
+			}
+			element.style.opacity = op;
+			op += op * 0.1;
+		}, 10);
 	};
 
 	toolTipElements.forEach(function(ele) {
 		ele.addEventListener("mouseenter", function(e) {
 			displayTooltip(e, this);
+		});
+		ele.addEventListener("mouseleave", function(e) {
+			// toolTipDiv.style.opacity = 0;
+			fadeOut(toolTipDiv);
 		});
 	});
 };
